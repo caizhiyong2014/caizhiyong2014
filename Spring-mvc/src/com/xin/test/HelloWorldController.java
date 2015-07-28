1fd90b24491b62982a6f70c6c4305d8ca531e387
caizhiyong2014 <1527816381@qq.com>
2015-07-28 19:26:08 +0800

    SpringMVC
    Demol

---
+++ Spring-mvc/src/com/xin/test/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.xin.test;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloWorldController implements Controller{
+
+	public ModelAndView handleRequest(HttpServletRequest arg0,
+			HttpServletResponse arg1) throws Exception {
+		ModelAndView mv=new ModelAndView();
+		mv.addObject("message", "hello world");
+		mv.setViewName("hello");
+		return mv;
+	}
+}
